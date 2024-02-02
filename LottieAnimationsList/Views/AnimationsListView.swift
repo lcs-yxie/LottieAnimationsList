5ca82a5071212fdcd166778421ddbe6b766932e3
Yukun Xie <yxie@lcs.on.ca>
2024-02-02 09:16:49 -0500

    Organized project and created a list view to show interesting animations.

---
+++ LottieAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationsList
+//
+//  Created by Yukun Xie on 2024/2/2.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationsListView()
+}
