1f7cd57a1dd6b222d1e9cc85bb99d51c368bd3ac
Yukun Xie <yxie@lcs.on.ca>
2024-02-02 09:21:05 -0500

    Added the third-party lottie-ios library.

--- LottieAnimationsList/LottieAnimationsListApp.swift
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LottieAnimationsListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AnimationsListView()
         }
     }
 }
